b5e8996bbdbe29e7bf55823df8dc6075e495ab25
Oleksandr <Oleksandr_Hrytsiuk@epam.com>
2021-05-22 21:15:22 +0300

    12

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,13 +1,13 @@
-import express, {Application, Request, Response, NextFunction} from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 
 const app: Application = express();
-const PORT = 3333   ;
+const PORT = 3333;
 
 const sayHello = () => console.log('Hello');
 
-app.get('/', (    req: Request, res: Response, next: NextFunction) => {
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
   sayHello();
-  res.send("Hello!"     );
+  res.send('Hello!');
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
